951296cbad6601fcf4d5c415a06ed62100d7f861
FkayRepo <99340187+FkayRepo@users.noreply.github.com>
2022-04-19 14:32:51 +0100

    Changing Database

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const imageURL = require('./controllers/imageURL');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      user : 'postgres',
-      password : 'sql',
-      database : 'facerecognitiondb'
+      host : process.env.DATABASE_URL,
+      ssl : true
     }
   });
 
