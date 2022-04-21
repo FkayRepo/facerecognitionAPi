35bba550d3ff94da56f096fa189d0c2af4e4035d
FkayRepo <99340187+FkayRepo@users.noreply.github.com>
2022-04-21 14:15:49 +0100

    Added extra layer of security

--- controllers/imageURL.js
+++ controllers/imageURL.js
@@ -11,7 +11,7 @@ const handleImageURLPost = (req,res,axios) => {
 	  headers: {
 	    'content-type': 'application/x-www-form-urlencoded',
 	    'X-RapidAPI-Host': 'ivladmin-face-detection.p.rapidapi.com',
-	    'X-RapidAPI-Key': 'cfa385174dmsh9d1c8475869f4aep1e336djsn7a4707ba4106'
+	    'X-RapidAPI-Key': process.env.API_key
 	  },
 	  data: encodedParams
 	};
